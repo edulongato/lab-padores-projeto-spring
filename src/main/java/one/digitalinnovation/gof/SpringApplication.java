0c590a788b4bb2fc6306d29ea9c540e1c22d2c31
Eduardo <edulongato@yahoo.com.br>
2021-11-03 23:18:03 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation/gof/SpringApplication.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class SpringApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+	}
+
+}
